a2a86ba4956a30c5c372415312ae9333c0782247
CJPobol <cjp25@njit.edu>
2022-10-28 13:35:12 -0400

    4 movement abilities; always active

--- src/player.c
+++ src/player.c
@@ -19,7 +19,7 @@ Entity *player_new(Vector3D position)
         return NULL;
     }
     
-    //ent->model = gf3d_model_load("dino");
+    ent->model = gf3d_model_load("cube");
     ent->think = player_think;
     ent->update = player_update;
     vector3d_copy(ent->position,position);
@@ -27,9 +27,11 @@ Entity *player_new(Vector3D position)
     return ent;
 }
 
+int crouching = 0;
 
 void player_think(Entity *self)
 {
+    crouching = 0;
     int radians = self->rotation.z;
     Vector3D forward;
     Vector3D right;
@@ -38,38 +40,53 @@ void player_think(Entity *self)
     const Uint8 * keys;
     keys = SDL_GetKeyboardState(NULL); // get the keyboard state for this frame
     int keyPressed = 0;
+    
 
     vector3d_angle_vectors(self->rotation, &forward, &right, &up);
-    vector3d_set_magnitude(&forward, 0.01);
-    vector3d_set_angle_by_radians(&forward, radians);
-    vector3d_set_magnitude(&right, 0.01);
-    vector3d_set_angle_by_radians(&right, radians - GFC_HALF_PI);
+    vector3d_set_magnitude(&forward, 0.001);
+    vector3d_set_angle_by_radians(&forward, -(radians - GFC_HALF_PI));
+    vector3d_set_magnitude(&right, 0.001);
+    vector3d_set_angle_by_radians(&right, radians);
     vector3d_set_magnitude(&up,0.1);
-    vector3d_set_magnitude(&dash, 1);
-    vector3d_set_angle_by_radians(&dash, radians);
+    vector3d_set_magnitude(&dash, 0.1);
+    vector3d_set_angle_by_radians(&dash, -(radians - GFC_HALF_PI));
 
     if (keys[SDL_SCANCODE_W])
     {   
         vector3d_add(self->position,self->position,forward);
+        if (self->position.x >= 100 || self->position.x <= -100 || self->position.y >= 100 || self->position.y <= -100)
+            self->position.z += 0.1;
     }
     if (keys[SDL_SCANCODE_S])
     {
-        vector3d_add(self->position,self->position,-forward);        
+        vector3d_add(self->position,self->position,-forward);
+        if (self->position.x >= 100 || self->position.x <= -100 || self->position.y >= 100 || self->position.y <= -100)
+            self->position.z += 0.1;
     }
     if (keys[SDL_SCANCODE_D])
     {
         vector3d_add(self->position,self->position,right);
+        if (self->position.x >= 100 || self->position.x <= -100 || self->position.y >= 100 || self->position.y <= -100)
+            self->position.z += 0.1;
     }
     if (keys[SDL_SCANCODE_A])    
     {
         vector3d_add(self->position,self->position,-right);
+        if (self->position.x >= 100 || self->position.x <= -100 || self->position.y >= 100 || self->position.y <= -100)
+            self->position.z += 0.1;
     }
 
+    //jump
+    if (keys[SDL_SCANCODE_SPACE])self->position.z = 10;
+    
 
-    if (keys[SDL_SCANCODE_SPACE])self->position.z = 100;
-    //else self->position.z = 0;
-    if (keys[SDL_SCANCODE_Z])self->position.z = -100;
-    //else self->position.z = 0;
+    if (keys[SDL_SCANCODE_Z])
+    {
+        self->position.z = -10;
+        crouching = 1;
+        vector3d_set_magnitude(&forward, 0.0001);
+        vector3d_set_magnitude(&right, 0.0001);
+    }
     
     if (keys[SDL_SCANCODE_UP])self->rotation.x -= 0.0050;
     if (keys[SDL_SCANCODE_DOWN])self->rotation.x += 0.0050;
@@ -85,10 +102,11 @@ void player_think(Entity *self)
         //radians += 0.0050;
     }
 
-    if (keys[SDL_SCANCODE_C] && keyPressed == 0)
+    if (keys[SDL_SCANCODE_C])
     {
-        vector3d_add(self->position, self->position, dash);
-        keyPressed = 1;
+        self->position.x = forward.x*100;
+        self->position.y = forward.y*100;
+        //vector3d_add(self->position, self->position, self->position);
     }
 }
 
@@ -97,11 +115,17 @@ void player_update(Entity *self)
     if (!self)return;
 
     //creates "gravity"
-    self->position.z -= 0.01;
+    self->position.z -= 0.08;
 
     //creates "floor" to game world
-    if (self->position.z <= 0)
-        self->position.z = 0;
+    if (crouching == 1)
+    {
+        if (self->position.z <= -10)
+            self->position.z = -10;
+    }
+    else
+        if (self->position.z <= 0)
+            self->position.z = 0;
 
 
     //creates "walls" to game world
